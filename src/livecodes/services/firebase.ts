193f0e7837fb4db787af4f205e9f78062d6a8c14
nunoluciano <1905497+gigamaster@users.noreply.github.com>
2024-09-30 01:59:11 +0200

    i18n spec

--- src/livecodes/services/firebase.ts
+++ src/livecodes/services/firebase.ts
@@ -4,11 +4,11 @@ export type { User as FirebaseUser } from 'firebase/auth';
 export { getAuth, signInWithPopup, signOut, GithubAuthProvider } from 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyB352dJ_NKCZ43G5kv9Lt-sb5nMXTJRONQ',
-  authDomain: 'livecodes-io.firebaseapp.com',
-  projectId: 'livecodes-io',
-  storageBucket: 'livecodes-io.appspot.com',
-  messagingSenderId: '756660932772',
-  appId: '1:756660932772:web:4cbbf58f809d81c2189631',
-  measurementId: 'G-C5WYBVEKF7',
+  apiKey: "AIzaSyCOcM4Q_lQsEEKdd-MYwRpJEthV5a1ZsH4",
+  authDomain: "livecodes-93937.firebaseapp.com",
+  projectId: "livecodes-93937",
+  storageBucket: "livecodes-93937.appspot.com",
+  messagingSenderId: "375700277019",
+  appId: "1:375700277019:web:955c205b7de97feaba75be",
+  measurementId: "G-XLCQFR6KFM"
 };
